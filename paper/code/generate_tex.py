78077d4fb0a506e7511478827636ffc9cbf8f133
Davide Fissore <fisso9900@gmail.com>
2024-03-22 09:43:44 +0100

    update python

--- paper/code/generate_tex.py
+++ paper/code/generate_tex.py
@@ -1,21 +1,24 @@
 import os, sys, re
 
 def get_file_cnt(lines):
+    res = []
     try:
         indexBegin = lines.index("%BEGIN\n")
         indexEnd = lines.index("%END\n")
-        return lines[indexBegin+1:indexEnd]
+        res = lines[indexBegin+1:indexEnd]
     finally:
-        return []
+        return res
 
 
 def print_tex(lines, fout):
     with open(fout, "w") as f:
         f.write("\\begin{elpicode}\n")
         for l in lines:
-            l = re.sub("^ *% +.*\n","",l)
-            l = re.sub("^ *%%%.*\n","",l)
-            l = re.sub("==l",r"~$\\Ue$~",l)
+            l = re.sub("^ *% +.*\n","",l)   
+            l = re.sub("^ *%SNIP.*\n","",l)   
+            l = re.sub("^ *%ENDSNIP.*\n","",l)   
+            l = re.sub("^ *%%%.*\n","",l)   
+            l = re.sub("==l",r"~$\\Ue$~",l) 
             l = re.sub("==o",r"~$\\Eo$~",l)
             l = re.sub(".*% *HIDE.*\n","",l)
             l = re.sub("type \(~\$([^ ]+)\$~\) (.*)\.$",r"~\\PYG{k+kd}{type} \\PYG{n+nf}{(\g<1>)} \\PYG{k+kt}{\g<2>}~.",l)
